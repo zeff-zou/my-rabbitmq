58c73b56dd3020f71e2e5a0751c1f3b831ea7e02
zouzhefu <371244694@qq.com>
2018-07-25 16:46:37 +0800

    add rabbitAdmin

---
+++ src/main/java/com/springbootdemo/config/RabbitMqConfig.java
@@ -0,0 +1,16 @@
+package com.springbootdemo.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+}
--- src/main/java/com/springbootdemo/delayqueue/DelayConsumer.java
+++ src/main/java/com/springbootdemo/delayqueue/DelayConsumer.java
@@ -1,18 +1,17 @@
 package com.springbootdemo.delayqueue;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Date;
+import java.util.HashMap;
 
 @Component
 public class DelayConsumer {
@@ -20,8 +19,8 @@ public class DelayConsumer {
     private final static String DELAY_QUEUE = "DELAY_QUEUE";//缓存队列(死信队列)
     private final static String DELAY_ROUTINGE = "DELAY_ROUTING";//缓存队列路由key
 
-    private final static String DELAY_CUSTOM_QUEUE = "DELAY_CUSTOM_QUEUE";//时间消费队列
-    private final static String DELAY_CUSTOM_ROUTING = "DELAY_CUSTOM_ROUTING";//时间消费队列路由key
+    private final static String DELAY_CUSTOM_QUEUE = "DELAY_CUSTOM_QUEUE";//实际消费队列
+    private final static String DELAY_CUSTOM_ROUTING = "DELAY_CUSTOM_ROUTING";//实际消费队列路由key
 
     private final static String DELAY_EXCHANGE = "DELAY_EXCHANGE";//交换器
 
@@ -40,25 +39,39 @@ public class DelayConsumer {
         System.out.println("receive date:"+new Date());
     }
 
-    //缓存队列(死信队列)
-    @Bean
-    public org.springframework.amqp.core.Queue delayCustomQueue(){
-        return QueueBuilder.durable(DELAY_QUEUE)
+    @Autowired
+    private RabbitAdmin rabbitAdmin;
+
+    @PostConstruct
+    public void initDelayCustomQueue(){
+        rabbitAdmin.declareQueue( QueueBuilder.durable(DELAY_QUEUE)
                 .withArgument("x-dead-letter-exchange", DELAY_EXCHANGE)//DLX deal letter(死信后转发的交换器)
                 .withArgument("x-dead-letter-routing-key", DELAY_CUSTOM_ROUTING)//死信后携带的路由
-                .build();
-    }
-    //缓存队列使用的交换机
-    @Bean
-    public org.springframework.amqp.core.Exchange delayExchange() {
-        return ExchangeBuilder.directExchange(DELAY_EXCHANGE).durable().build();
+                .build());
+        rabbitAdmin.declareExchange(ExchangeBuilder.directExchange(DELAY_EXCHANGE).durable().build());
+        rabbitAdmin.declareBinding(new Binding(DELAY_QUEUE,Binding.DestinationType.QUEUE,
+                DELAY_EXCHANGE,DELAY_ROUTINGE,new HashMap()));
     }
 
-    //将缓存队列和交换机以及路由key绑定起来
-    @Bean
-    public Binding myExchangeBinding(@Qualifier("delayExchange") org.springframework.amqp.core.Exchange directExchange,
-                                     @Qualifier("delayCustomQueue") org.springframework.amqp.core.Queue queue) {
-        return BindingBuilder.bind(queue).to(directExchange).with(DELAY_ROUTINGE).noargs();
-    }
+    //    //缓存队列(死信队列)
+//    @Bean
+//    public org.springframework.amqp.core.Queue delayCustomQueue(){
+//        return
+//
+//
+//
+//    }
+//    //缓存队列使用的交换机
+//    @Bean
+//    public org.springframework.amqp.core.Exchange delayExchange() {
+//        return ExchangeBuilder.directExchange(DELAY_EXCHANGE).durable().build();
+//    }
+//
+//    //将缓存队列和交换机以及路由key绑定起来
+//    @Bean
+//    public Binding myExchangeBinding(@Qualifier("delayExchange") org.springframework.amqp.core.Exchange directExchange,
+//                                     @Qualifier("delayCustomQueue") org.springframework.amqp.core.Queue queue) {
+//        return BindingBuilder.bind(queue).to(directExchange).with(DELAY_ROUTINGE).noargs();
+//    }
 
 }
