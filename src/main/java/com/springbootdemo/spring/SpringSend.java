7e77a7ad275bb0a8619132d9557cbe955849e840
zouzhefu <371244694@qq.com>
2018-07-25 14:16:31 +0800

    fix

--- src/main/java/com/springbootdemo/spring/SpringSend.java
+++ src/main/java/com/springbootdemo/spring/SpringSend.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
 
 @Component
 public class SpringSend {
