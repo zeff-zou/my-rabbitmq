ddf68b78809557830c87f0658f3e1b3a1f2a4321
zouzhefu <371244694@qq.com>
2018-07-31 17:57:26 +0800

    fix

--- src/main/java/com/springbootdemo/delayqueue/plugin/DelayPluginConsumer.java
+++ src/main/java/com/springbootdemo/delayqueue/plugin/DelayPluginConsumer.java
@@ -18,23 +18,26 @@ public class DelayPluginConsumer {
 
     public static final String DELAY_PLUGIN_QUEUE = "DELAY_PLUGIN_QUEUE";
     public static final String DELAY_PLUGIN_EXCHANGE = "DELAY_PLUGIN_EXCHANGE";
-    private final static String DELAY_PLUGIN_ROUTING = "DELAY_PLUGIN_ROUTING";//实际消费队列路由key
+    private final static String DELAY_PLUGIN_ROUTING = "DELAY_PLUGIN_ROUTING";
 
     @Autowired
     private RabbitAdmin rabbitAdmin;
 
     @PostConstruct
     public void init(){
+        //创建队列
+        rabbitAdmin.declareQueue(QueueBuilder.durable(DELAY_PLUGIN_QUEUE).build());
+        //创建交换机(由于Spring AMQP中没有这个类型的交换机，所以我们使用一个CustomExchange来定义这个插件构建的交换机)
         Map<String, Object> args = new HashMap<String, Object>();
         args.put("x-delayed-type", "direct");
-        rabbitAdmin.declareQueue(QueueBuilder.durable(DELAY_PLUGIN_QUEUE).build());
         rabbitAdmin.declareExchange(new CustomExchange(DELAY_PLUGIN_EXCHANGE, "x-delayed-message", true, false, args));
+        //绑定交换机与队列
         rabbitAdmin.declareBinding(new Binding(DELAY_PLUGIN_QUEUE,Binding.DestinationType.QUEUE,
                 DELAY_PLUGIN_EXCHANGE,DELAY_PLUGIN_ROUTING,new HashMap()));
     }
 
 
-    //消费队列
+    //监听消费队列
     @RabbitListener(queues  = DELAY_PLUGIN_QUEUE)
     public void process(String message){
         System.out.println("Message:"+message);
